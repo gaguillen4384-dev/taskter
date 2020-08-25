6b20ee3f494329d98c21a711acacff77a430c341
KaliGuillens <gaguillen4384-dev@gmail.com>
2020-08-24 21:26:12 -0400

    Initial Skeleton of utility tool

---
+++ Taskter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taskter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
